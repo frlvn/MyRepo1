50a60b3f8113aa2ffaa56dab2225502acc664f65
frlvn <126551835+frlvn@users.noreply.github.com>
2023-04-10 16:45:33 +0300

    Add files via upload

---
+++ lab6/main/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def text(request):
+    text = "This is a sample text. It contains multiple words, some of which are repeated multiple times. Some words may appear in different cases like uppercase and lowercase, but we need to count them as the same word."
+
+    # Преобразуем весь текст в нижний регистр для сравнения слов
+    text_lower = text.lower()
+
+    # Разбиваем текст на отдельные слова
+    words = text_lower.split()
+
+    # Используем множество для хранения уникальных слов
+    unique_words = set(words)
+
+    return HttpResponse(list(unique_words))
